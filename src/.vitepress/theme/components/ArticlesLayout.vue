0484fcc1b0fb8f54776cc34fcdc5c99249ca3858
Nicholas Lim <nicknitewolf@lineageos.org>
2024-01-07 01:42:31 +0800

    break on smaller width

--- src/.vitepress/theme/components/ArticlesLayout.vue
+++ src/.vitepress/theme/components/ArticlesLayout.vue
@@ -36,16 +36,20 @@ const filteredPosts = computed(() => {
           <p class="mt-2 text-gray-500 dark:text-brand-soft">
             {{ post.excerpt }}
           </p>
-          <p class="mt-2 text-gray-800 dark:text-white flex align-middle">
-            <span class="material-symbols-outlined mr-1 text-[1.4rem]">
-              calendar_today
-            </span>
-            {{ post.date.string }}&nbsp;
-            <span class="material-symbols-outlined mr-1 text-[1.4rem]">
-              person
-            </span>
-            {{ post.author }}
-          </p>
+          <div class="mt-2 text-gray-800 dark:text-white flex flex-wrap">
+            <div class="flex flex-grow-0 items-center">
+              <span class="material-symbols-outlined mr-1 text-[1.4rem]">
+                calendar_today
+              </span>
+              {{ post.date.string }}&nbsp;
+            </div>
+            <div class="flex flex-grow items-center">
+              <span class="material-symbols-outlined mr-1 text-[1.4rem]">
+                person
+              </span>
+              {{ post.author }}
+            </div>
+          </div>
           <a
             class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-brand-2 hover:text-brand-3 disabled:opacity-50 disabled:pointer-events-none dark:text-brand-soft dark:hover:text-brand-1 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
             :href="post.url"
