3c2ed238e2c0af39b4da097e97a5b4cebaf24b27
Nicholas Lim <nicknitewolf@lineageos.org>
2024-01-06 14:54:19 +0800

    Use brand fonts - Roboto

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,7 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme-without-fonts'
+import '@fontsource/roboto'
+import "@fontsource/roboto-mono"
 import './style.css'
 
 export default {
